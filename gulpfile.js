3eda89c753f50993d04258a71034795ab191086f
Matt Vella <mattvella07@gmail.com>
2016-11-03 23:09:09 -0500

    Updated gulpfile to remove unused packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
 var gulp = require('gulp'),
     react = require('gulp-react'),
-    babel = require('gulp-babel'),
     jshint = require('gulp-jshint'),
     less = require('gulp-less'),
     uglify = require('gulp-uglifyjs'),
     cleanCSS = require('gulp-clean-css'),
     rename = require('gulp-rename'),
     del = require('del'),
-    concat = require('gulp-concat');
-
-var browserify = require('browserify'),
+    browserify = require('browserify'),
     babelify = require('babelify'),
-    source = require('vinyl-source-stream');
+    source = require('vinyl-source-stream'),
+    streamify = require('gulp-streamify');
 
 var jsPaths = ['./*.js', './src/components/*.jsx', './src/*.jsx', './lib/*.js', './lib/routes/*.js', './lib/routes/test/*.js'];
 
@@ -43,22 +41,7 @@ gulp.task('lint', function() {
     });
 });
 
-//Minify Javascript and copy it to dist directory 
-//Original
-/*gulp.task('minifyJS', function() {
-    return gulp.src('./src/*.jsx')
-        .pipe(react({es6module: true}))
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist'));
-});*/
-
-//New
+//Minify JavaScript and move it to dist folder 
 gulp.task('minifyJS', function () {
   browserify({
     entries: 'src/app.jsx',
@@ -68,6 +51,7 @@ gulp.task('minifyJS', function () {
   .transform(babelify)
   .bundle()
   .pipe(source('app.min.js'))
+  .pipe(streamify(uglify()))
   .pipe(gulp.dest('dist'));
 });
 
@@ -78,6 +62,7 @@ gulp.task('watch', function() {
     gulp.watch('./src/*.jsx', ['minifyJS']);
 });
 
+//Initial task 
 gulp.task('default', ['clean'], function() {
     gulp.start('less', 'lint', 'minifyJS', 'watch');
 });
