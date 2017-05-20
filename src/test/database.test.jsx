aa5631fc085bf1a0f94115dc9b0fd9167adc4c33
Matt Vella <mattvella07@gmail.com>
2017-05-20 08:40:19 -0500

    Started test for database component

---
+++ src/test/database.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import expect from 'expect';
+import Database from './../components/database.jsx';
+
+describe('Database', () => {
+    let props, 
+        mountedDatabase;
+
+    beforeEach(() => {
+        
+    });
+});
--- src/test/databaseList.test.jsx
+++ src/test/databaseList.test.jsx
@@ -1,20 +1,26 @@
 /*
     -A div is always rendered with a class name of databaseList
     -An h3 element is always rendered with the text "DATABASES"
-    -PropType source should be a string
-    -PropType onDBClick should be a func
     -If a Database is rendered it should receieve exactly 1 prop 
     -Initial state of dbNames should equal ''
-
 */
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import expect from 'expect';
 import DatabaseList from './../components/databaseList.jsx';
 
 describe('DatabaseList', () => {
-    let props;
-    let mountedDatabaseList;
+    let props,
+        mountedDatabaseList;
+
+    beforeEach(() => {
+        props = {
+            source: 'http://localhost:3000/api/databases',
+            onDBClick: function() {}
+        };
+        mountedDatabaseList = undefined;
+    });
+
     const databaseList = () => {
         if(!mountedDatabaseList) {
             mountedDatabaseList = mount(
@@ -25,36 +31,31 @@ describe('DatabaseList', () => {
         return mountedDatabaseList;
     }
 
-    beforeEach(() => {
-        props = {
-            source: 'http://localhost:3000/api/databases',
-            onDBClick: function() {}
-        };
-        mountedDatabaseList = undefined;
-    });
-
-    const divs = databaseList().find("div");
-
     it("always returns a div", () => {
+        const divs = databaseList().find("div");
         expect(divs.length).toBeGreaterThan(0);
     });
 
     it("div has the class databaseList", () => {
+        const divs = databaseList().find("div");
         expect(divs.hasClass('databaseList')).toEqual(true);
     });
 
     it("displays an H3 element", () => {
+        const divs = databaseList().find("div");
         const header = divs.find("h3");
         expect(header.length).toBeGreaterThan(0);
     });
 
     it("H3 element has the text 'DATABASES'", () => {
+        const divs = databaseList().find("div");
         const header = divs.find("h3");
         expect(header.text()).toEqual('DATABASES');
     });
 
     /*it("database ", () => {
-        const db = divs.find(".database");
+        const divs = databaseList().find("div");
+        const db = divs.find("Database");
         if(db.length > 0) {
             console.log('---' + db.length);
             expect(db.hasClass('database')).toEqual(true);
